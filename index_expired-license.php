c840f4f5e82354d2e9c77043e1fbdc44bdef8cf7
JaySoftye <jonathansoftye@teq.com>
2020-06-25 08:40:46 -0400

    New home page popup added
    Updated modal code added

---
+++ index_expired-license.php
@@ -0,0 +1,33 @@
+<?php include 'header.php';?>
+
+<div class="modal is-active show" id="login-window" tabindex="-1" role="dialog" aria-labelledby="login-window" aria-modal="true" style="padding-right: 15px; display: block;">
+  <div class="hover_bkgr_covid" style="display: block; background: rgba(0,0,0,.7); height: 100%; position: fixed; text-align: center; top: 0; width: 100%; z-index: 10000; overflow-y: scroll; overflow-x: hidden;">
+
+    <!-- popup text to copy -->
+
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content login-container">
+        <section class="login-section login-card">
+          <div class="login-form">
+            <img src="assets/images/covidPopExtendYour-License-OTIS.svg" style="margin-top: -6rem;"/>
+            <div class="main-login-form margin-bottom">
+              <br />
+              <h4 class="thin-heading bold text-center">If you are currently using OTIS for educators for FREE, your <u>access to our PD may be ENDING on JUNE 30th.</u></h4>
+              <p class="text-center"><a href="https://www.teq.com/Otis-for-gsuite-admins/">Contact us</a> about extending your subscription or purchase a discounted license here</p>
+              <a href="https://www.teq.com/Otis-for-gsuite-admins/">
+                <button type="button" class="login">EXTEND YOUR LICENSE TODAY</button>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <section class="login-section login-card">
+          <a href="https://otis.teq.com/users/register/individual">
+            <img src="assets/images/covidPopIndividual-license-Purchase.jpg" alt="If access is not extended, you will still be able to purchase courses or sign up as an individual here" />
+          </a>
+        </section>
+
+      </div>
+    </div>
+  </div>
+</div>
