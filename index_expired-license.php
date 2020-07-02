498db40497cb6a684cfcdd207ad89659f03a9ecb
JaySoftye <jonathansoftye@teq.com>
2020-07-02 15:54:57 -0400

    Updated Popup
    Single course purchase | Individual License

--- index_expired-license.php
+++ index_expired-license.php
@@ -12,8 +12,8 @@
             <img src="assets/images/covidPopExtendYour-License-OTIS.svg" style="margin-top: -6rem;"/>
             <div class="main-login-form margin-bottom">
               <br />
-              <h4 class="thin-heading bold text-center">We hope you enjoyed free access<br />to OTIS for educators!</u></h4>
-              <p class="text-center">Keep learning with us and prepare for the new school year when you <a href="https://www.teq.com/Otis-for-gsuite-admins/"><u>extend your license.</u></a></p>
+              <h4 class="thin-heading bold text-center">We hope you enjoyed your FREE access to OTIS for educators!</u></h4>
+              <h6 class="text-center">Keep learning with us and prepare for the new school year when you <a href="https://www.teq.com/Otis-for-gsuite-admins/"><u>extend your license.</u></a></h6>
               <a href="https://www.teq.com/Otis-for-gsuite-admins/">
                 <button type="button" class="login">PURCHASE A DISCOUNTED LICENSE TODAY</button>
               </a>
@@ -22,9 +22,20 @@
         </section>
 
         <section class="login-section login-card">
-          <a href="https://otis.teq.com/users/register/individual">
-            <img src="assets/images/covidPopIndividual-license-Purchase.jpg" alt="If access is not extended, you will still be able to purchase courses or sign up as an individual here" />
-          </a>
+          <div class="padding-sm">
+            <div class="main-login-form">
+              <h4 class="thin-heading bold text-center">All educators welcome!</h4>
+              <h6 class="text-center">We want you to continue to learn with us.<br /><strong>Use your existing account topurchase single stand alone courses.</strong></h6>
+              <a href>
+                <img class="width-full" src="assets/images/add-to-cart_browse-courses-button.jpg" alt="Browse Courses" />
+              </a>
+              <br /><br />
+              <h5 class="text-center"><strong>or SAVE on an annual individual license</strong><br />when you subscribe to OTIS for educators.</h5>
+              <a href="https://otis.teq.com/pricing">
+                <img class="width-full" src="assets/images/save-on-your-subscription_register-button.jpg" alt="Register" />
+              </a>
+            </div>
+          </div>
         </section>
 
       </div>
