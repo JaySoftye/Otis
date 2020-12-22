a38601e8e93ab8b568e54ca01b71ea4b5cf38ef1
JaySoftye <jonathansoftye@teq.com>
2020-12-22 08:26:08 -0500

    Startup page
    smart board startup pages created

---
+++ smart-startup-nyc.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMART Start with Teq and OTIS for educators</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/pih4zrr.css">
+    <link href="https://otis.teq.com/assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+    <link href="https://otis.teq.com/assets/css/shared.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://otis.teq.com/assets/js/bootstrap.min.js"></script>
+
+
+    <style>
+      body main { height:auto; width:auto; padding:1.5rem 1.75rem; background-size:cover; background-repeat:no-repeat; background-position:center center; background-image:url('./assets/images/smart-startup_background_2560x1440.jpg'); }
+      .text-blue { color:rgb(140,188,215); }
+      .text-green { color:rgb(169,198,130); }
+      .text-white { color:rgb(255,255,255); }
+      .square { height:0; width:75%; position:absolute; top:0; margin:0 15px 0 0; padding-top:75%; background-color: transparent; border-radius:6px; }
+      .square.right { right:0; }
+      .square.left { left:0; }
+      .square > .content {position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); text-align:center; font-weight:800; font-size:1.2rem; color:#FFFFFF; }
+      .square.red { background-color:rgb(188,83,91); }
+      .square.blue { background-color:rgb(140,188,215); }
+      .square.green { background-color:rgb(169,198,130); }
+      .white-border { border:1px solid #FFFFFF; }
+      .course-card-container { margin-top:1rem; padding-right:0; }
+      .course-card-container > .card.course-card { border:none; }
+      .course-card-container > .card.course-card > .card-header { background-color:rgb(169,198,130); }
+      .nav-item.category-duration-details > svg { height:27px; width:27px; }
+      .nav-item.category-duration-details > a { color:#FFFFFF; }
+      .course-card-container > .card.course-card > .card-body { padding:1rem; }
+      .card-body p { margin-bottom:.36rem; }
+      .card-body p a { line-height:1.2; font-size:1.5rem; color:#000000; }
+
+    </style>
+
+</head>
+<body>
+
+  <main>
+    <section class="container-fluid">
+      <div class="row">
+
+
+        <div class="col-6">
+          <div class="row">
+            <div class="col-3">
+              <?php echo file_get_contents('./assets/images/smart-startup_powered_by_teq.svg'); ?>
+            </div>
+            <div class="col-3">
+              <?php echo file_get_contents('./assets/images/smart-startup_NYC-department-of-ed.svg'); ?>
+            </div>
+            <div class="col-2">
+              <a class="square right red" href="https://www.schools.nyc.gov/calendar?event_categories=1|Key%20Dates">
+                <div class="content">Events<br />Calendar</div>
+              </a>
+            </div>
+            <div class="col-2">
+              <a class="square left red" href="https://infohub.nyced.org/">
+                <div class="content">Info<br />Hub</div>
+              </a>
+            </div>
+          </div>
+          <h1 class="mt-3 display-4 text-blue"><strong>Get to know your SMART Board</strong></h1>
+          <h2 class="h3 mt-3 text-white"><b>Here are a few quick links to help get you started.</b></h2>
+          <div class="row no-margin">
+            <div class="col-5 white-border mt-3 d-flex align-items-center p-3">
+              <script src="https://fast.wistia.com/embed/medias/qxmnbhhtdo.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+              <div class="wistia_responsive_padding">
+                <div class="wistia_responsive_wrapper">
+                  <span class="wistia_embed wistia_async_qxmnbhhtdo popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                    <a class="h4" href="#">How to connect a computer to a smart Board?</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-5 white-border mt-3 ml-3 d-flex align-items-center p-3">
+              <script src="https://fast.wistia.com/embed/medias/am7c622tpd.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+              <div class="wistia_responsive_padding">
+                <div class="wistia_responsive_wrapper">
+                  <span class="wistia_embed wistia_async_am7c622tpd popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                    <a class="h4" href="#">Signing into your SMART account!</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row no-margin">
+            <div class="col-5 white-border mt-3 d-flex align-items-center p-3">
+              <script src="https://fast.wistia.com/embed/medias/x2c2zosnbv.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+              <div class="wistia_responsive_padding">
+                <div class="wistia_responsive_wrapper">
+                  <span class="wistia_embed wistia_async_x2c2zosnbv popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                    <a class="h4" href="#">How to use SMART ink?</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-5 white-border mt-3 ml-3 d-flex align-items-center p-3">
+              <a class="h4" href="#">How to clean a SMART board?</a>
+            </div>
+          </div>
+          <div class="row no-margin">
+            <div class="col-5 white-border mt-3 d-flex align-items-center p-3">
+              <script src="https://fast.wistia.com/embed/medias/j5do4yhmhy.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+              <div class="wistia_responsive_padding">
+                <div class="wistia_responsive_wrapper">
+                  <span class="wistia_embed wistia_async_j5do4yhmhy popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                    <a class="h4" href="#">How to use Screen Share?</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-5 white-border mt-3 ml-3 d-flex align-items-center p-3">
+              <script src="https://fast.wistia.com/embed/medias/z4lu3kryvi.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+              <div class="wistia_responsive_padding">
+                <div class="wistia_responsive_wrapper">
+                  <span class="wistia_embed wistia_async_z4lu3kryvi popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                    <a class ="h4" href="#">Your iQ experience!</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-4 mt-3 d-flex align-items-center">
+              <h2 class="h3 text-white"><strong>Having an issue?<br />Call for Teq Support:</strong></h2>
+            </div>
+            <div class="col-6 mt-3 ml-3 d-flex align-items-center p-3">
+              <h1 class="text-blue"><strong>877.455.9369</strong></h1>
+            </div>
+          </div>
+          <div class="row mt-5 mb-5">
+            <div class="col-2">
+              <a class="square right blue" href="https://www.teq.com/">
+                <div class="content">Visit<br />teq.com</div>
+              </a>
+            </div>
+            <div class="col-7">
+              <h5 class="text-white">Teq’s <strong class="text-blue">“The Complete Thought”</strong> brings together all the dynamic, moving parts of education so that no matter how varied your solutions are, you have a big picture outlook, and the tools to help you reach that goal. </h5>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-6">
+
+          <div class="row">
+            <div class="col-3">
+              <?php echo file_get_contents('./assets/images/smart-startup_OTIS-mascot.svg'); ?>
+            </div>
+            <div class="col">
+              <?php echo file_get_contents('./assets/images/smart-startup_OTIS-for-educators.svg'); ?>
+              <h2 class="h5 mt-3 text-white">Here are a few instructional sessions from <strong class="text-green">OTIS for educators</strong> to provide guidance on how to get started with this SMART Board.</h2>
+            </div>
+          </div>
+
+          <div class="row d-flex align-items-center">
+            <div class="col-3">
+              <h5 class="text-white"><strong>Select a course tile to get started:</strong></h5>
+            </div>
+            <div class="col">
+              <div class="row nomargin">
+                <div class="col-6 course-card-container">
+                  <div class="card course-card ">
+                    <div class="card-header product-familiarization">
+                      <ul class="nav course-details">
+                        <li class="nav-item category-duration-details">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300">
+                            <path fill="#FFFFFF" d="M118.446,33.93C54.213,33.93,2.142,86.002,2.142,150.235S54.213,266.54,118.446,266.54 s116.305-52.071,116.305-116.305S182.68,33.93,118.446,33.93z M170.371,218.159c-1.855,1.854-4.286,2.782-6.718,2.782 s-4.862-0.928-6.718-2.782l-55.989-55.989V81.001c0-5.247,4.253-9.5,9.5-9.5s9.5,4.253,9.5,9.5V154.3l50.425,50.423 C174.081,208.434,174.081,214.449,170.371,218.159z"/>
+                          </svg>
+                        </li>
+                        <li class="nav-item category-duration-details">
+                          <a href="#">16min</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <script src="https://fast.wistia.com/embed/medias/y3h25ot91i.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                    <div class="wistia_responsive_padding">
+                      <div class="wistia_responsive_wrapper">
+                        <span class="wistia_embed wistia_async_y3h25ot91i popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                          <div class="card-body">
+                            <p><a href="#">Overview of SMART Board Basics </a></p>
+                            <p class="card-text">Learn to share content to your students, and create a hub of collaboration...</p>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 course-card-container">
+                  <div class="card course-card ">
+                    <div class="card-header product-familiarization">
+                      <ul class="nav course-details">
+                        <li class="nav-item category-duration-details">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300">
+                            <path fill="#FFFFFF" d="M118.446,33.93C54.213,33.93,2.142,86.002,2.142,150.235S54.213,266.54,118.446,266.54 s116.305-52.071,116.305-116.305S182.68,33.93,118.446,33.93z M170.371,218.159c-1.855,1.854-4.286,2.782-6.718,2.782 s-4.862-0.928-6.718-2.782l-55.989-55.989V81.001c0-5.247,4.253-9.5,9.5-9.5s9.5,4.253,9.5,9.5V154.3l50.425,50.423 C174.081,208.434,174.081,214.449,170.371,218.159z"/>
+                          </svg>
+                        </li>
+                        <li class="nav-item category-duration-details">
+                          <a href="#">35min</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <script src="https://fast.wistia.com/embed/medias/u1lzdlsgwi.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                    <div class="wistia_responsive_padding">
+                      <div class="wistia_responsive_wrapper">
+                        <span class="wistia_embed wistia_async_u1lzdlsgwi popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                          <div class="card-body">
+                            <p><a href="#">Introduction to SMART Notebook</a></p>
+                            <p class="card-text">Curious to learn how Notebook can help you take your teaching to the next level?</p>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 course-card-container">
+                  <div class="card course-card ">
+                    <div class="card-header product-familiarization">
+                      <ul class="nav course-details">
+                        <li class="nav-item category-duration-details">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300">
+                            <path fill="#FFFFFF" d="M118.446,33.93C54.213,33.93,2.142,86.002,2.142,150.235S54.213,266.54,118.446,266.54 s116.305-52.071,116.305-116.305S182.68,33.93,118.446,33.93z M170.371,218.159c-1.855,1.854-4.286,2.782-6.718,2.782 s-4.862-0.928-6.718-2.782l-55.989-55.989V81.001c0-5.247,4.253-9.5,9.5-9.5s9.5,4.253,9.5,9.5V154.3l50.425,50.423 C174.081,208.434,174.081,214.449,170.371,218.159z"/>
+                          </svg>
+                        </li>
+                        <li class="nav-item category-duration-details">
+                          <a href="#">36min</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <script src="https://fast.wistia.com/embed/medias/bghjj9e5py.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                    <div class="wistia_responsive_padding">
+                      <div class="wistia_responsive_wrapper">
+                        <span class="wistia_embed wistia_async_bghjj9e5py popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                          <div class="card-body">
+                            <p><a href="#">Use Your SMART Board Without a Computer</a></p>
+                            <p class="card-text">Explore the brand new features of the SMART iQ!</p>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 course-card-container">
+                  <div class="card course-card ">
+                    <div class="card-header product-familiarization">
+                      <ul class="nav course-details">
+                        <li class="nav-item category-duration-details">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300">
+                            <path fill="#FFFFFF" d="M118.446,33.93C54.213,33.93,2.142,86.002,2.142,150.235S54.213,266.54,118.446,266.54 s116.305-52.071,116.305-116.305S182.68,33.93,118.446,33.93z M170.371,218.159c-1.855,1.854-4.286,2.782-6.718,2.782 s-4.862-0.928-6.718-2.782l-55.989-55.989V81.001c0-5.247,4.253-9.5,9.5-9.5s9.5,4.253,9.5,9.5V154.3l50.425,50.423 C174.081,208.434,174.081,214.449,170.371,218.159z"/>
+                          </svg>
+                        </li>
+                        <li class="nav-item category-duration-details">
+                          <a href="#">42min</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <script src="https://fast.wistia.com/embed/medias/4se8a03oeh.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+                    <div class="wistia_responsive_padding">
+                      <div class="wistia_responsive_wrapper">
+                        <span class="wistia_embed wistia_async_4se8a03oeh popover=true popoverAnimateThumbnail=true popoverContent=link videoFoam=true">
+                          <div class="card-body">
+                            <p><a href="#">Explore Your SMART Learning Suite</a></p>
+                            <p class="card-text">Classroom activities and enhance archived lessons to extend learning.</p>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <h5 class="mt-3 text-white">Join over 600,000 of your peers worldwide utilizing <strong class="text-green">OTIS for educators</strong> for professional development on technology, tools, and strategies that sparks student success. Scan the QR code and join today.</h5>
+            </div>
+          </div>
+
+          <div class="row mt-4 mb-5">
+            <div class="col-3 offset-3">
+              <h2 class="h3 mt-3 text-white"><b>FREE SMART Board courses*</b></h2>
+              <p><small class="text-white">*Sign up for a FREE Basic User account for access.</small></p>
+            </div>
+            <div class="col-2 no-padding">
+              <a class="square green" href="https://otis.teq.com/courses/category/role/2/type/1?q=">
+                <div class="content">FREE<br />COURSES</div>
+              </a>
+            </div>
+            <div class="col-3">
+              <?php echo file_get_contents('./assets/images/smart-startup_sign-up-for-otis.svg'); ?>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+    </section>
+  </main>
+
+</body>
+</html>
